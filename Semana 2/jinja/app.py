898d69e3864d8ab5bdcf5a9dada924e8f3c39d94
Damas-WISH <141933586+Damas-WISH@users.noreply.github.com>
2025-08-07 00:45:33 +0000

    novos updates

--- Semana 2/jinja/app.py
+++ Semana 2/jinja/app.py
@@ -7,6 +7,19 @@ def index():
     usuario = 'Nicolas Damas Wischral'
     return render_template('index.html', nome_usuario = usuario)
 
+@app.route('/perfil/<nome>')
+def perfil (nome):
+    logado = True
+    usuario = nome
+    return render_template('perfil.html', logado=logado,nome_usuario = usuario)
+
+@app.route('/lista_produtos')
+def lista_produtos():
+    produtos = ['cama','coberta','travesseiro']
+    return render_template('produtos.html', lista=produtos)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
+
+
 
