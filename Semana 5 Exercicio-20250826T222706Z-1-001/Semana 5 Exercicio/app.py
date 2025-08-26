94b770fa05cacb8f4aa395e798720a712c8680a4
Damas-WISH <141933586+Damas-WISH@users.noreply.github.com>
2025-08-26 19:28:05 -0300

    Adição

---
+++ Semana 5 Exercicio-20250826T222706Z-1-001/Semana 5 Exercicio/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+app = Flask(__name__)
+app.secret_key = "segredo123"  
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/nova-receita", methods=["GET", "POST"])
+def nova_receita():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        ingredientes = request.form.get("ingredientes")
+        preparo = request.form.get("preparo")
+
+        
+        if not nome or not ingredientes or not preparo:
+            flash("Todos os campos são obrigatórios!", "erro")
+            return redirect(url_for("nova_receita"))
+
+        
+        session["nome"] = nome
+        session["ingredientes"] = ingredientes
+        session["preparo"] = preparo
+
+        return redirect(url_for("receita_criada"))
+
+    return render_template("receita.html")
+
+@app.route("/receita-criada")
+def receita_criada():
+    nome = session.get("nome")
+    ingredientes = session.get("ingredientes")
+    preparo = session.get("preparo")
+
+    return render_template("receita_criada.html",
+                           nome=nome,
+                           ingredientes=ingredientes,
+                           preparo=preparo)
+
+if __name__ == "__main__":
+    app.run(debug=True)
